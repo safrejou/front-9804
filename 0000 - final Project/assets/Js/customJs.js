542f4727b7213f89f48e42071fe2b5c3bcc84cf0
homan2070 <homan2070@gmdil.com>
2019-10-03 22:20:20 +0330

    sliderJs

--- 0000 - final Project/assets/Js/customJs.js
+++ 0000 - final Project/assets/Js/customJs.js
@@ -1,24 +1,82 @@
 $(document).ready(function () {
 
-// ------- 10 - form section - submit botton -------
+    // ---------- 6 - slider --------------
+    var slideCount = $(".slider ul li").length;
 
-    var name = document.getElementById("name");
-    var email = document.getElementById("e-mail");
-    var subject = document.getElementById("subject");
-    var comment = document.getElementById("comment");
+    var slideWidth = $(".slider ul li").width();
 
-    document.querySelector('form.user-info').addEventListener('submit', function (e) {
+    var slideHeight = $(".slider ul li").height();
 
-        //prevent the normal submission of the form
-        e.preventDefault();
+    var slideArea = $ (".main");
 
-        console.log(name.value);
-        console.log(email.value);
-        console.log(subject.value);
-        console.log(comment.value);
+    var sliderUlWidth = slideCount * slideWidth;
 
+
+    $(".slider").css({
+        width: slideArea,
+        height: slideHeight
     });
 
+    $(".slider ul").css({
+        width: sliderUlWidth,
+        marginLeft: -slideWidth
+    });
+
+    $(".slider ul li:last-child").prependTo(".slider ul");
+
+
+    function moveLeft() {
+        $(".slider ul").animate({
+            right: +slideWidth
+        }, 300, function () {
+            $(".slider ul li:last-child").prependTo(".slider ul");
+            $(".slider ul").css('right', '0');
+        });
+    }
+
+    function moveRight(){
+        $(".slider ul").animate({
+            left: -slideWidth
+        },500,function(){
+            $(".slider ul li:first-child").appendTo(".slider ul");
+            $(".slider ul").css('left', '0');
+        });
+    }
+
+    $("a.control-next").click(function (e) {
+        moveRight();
+        return false;
+    });
+
+    $("a.control-pre").click(function (e) {
+        moveLeft();
+        return false;
+    });
+
+
+// ---------- /6 - slider --------------
+
+
 // ------- 10 - form section - submit botton -------
+
+var name = document.getElementById("name");
+var email = document.getElementById("e-mail");
+var subject = document.getElementById("subject");
+var comment = document.getElementById("comment");
+
+document.querySelector('form.user-info').addEventListener('submit', function (e) {
+
+    //prevent the normal submission of the form
+    e.preventDefault();
+
+
+    console.log(name.value);
+    console.log(email.value);
+    console.log(subject.value);
+    console.log(comment.value);
+
+});
+
+// ------- /10 - form section - submit botton -------
 
 });
