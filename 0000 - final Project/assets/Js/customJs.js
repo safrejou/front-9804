8bae517678bfaf3090c5aa70903f030ed70cce4a
homan2070 <homan2070@gmdil.com>
2019-10-04 12:11:56 +0330

    final

--- 0000 - final Project/assets/Js/customJs.js
+++ 0000 - final Project/assets/Js/customJs.js
@@ -52,30 +52,81 @@ $(document).ready(function () {
         moveLeft();
         return false;
     });
-
-
 // ---------- /6 - slider --------------
+// -------- 6 - slider 2 --------------
+    var slideCount2 = $(".slider-2 ul li").length;
+
+    var slideWidth2 = $(".slider-2 ul li").width();
+
+    var slideHeight2 = $(".slider-2 ul li").height();
+
+    var slideArea2 = $ (".main");
+
+    var sliderUlWidth2 = slideCount * slideWidth;
+
+
+    $(".slider-2").css({
+        width: slideArea2,
+        height: slideHeight2
+    });
+
+    $(".slider-2 ul").css({
+        width: sliderUlWidth2,
+        marginLeft: -slideWidth2
+    });
+
+    $(".slider-2 ul li:last-child").prependTo(".slider-2 ul");
+
+
+    function moveLeft2() {
+        $(".slider-2 ul").animate({
+            right: +slideWidth2
+        }, 300, function () {
+            $(".slider-2 ul li:last-child").prependTo(".slider-2 ul");
+            $(".slider-2 ul").css('right', '0');
+        });
+    }
+
+    function moveRight2(){
+        $(".slider ul").animate({
+            left: -slideWidth2
+        },500,function(){
+            $(".slider-2 ul li:first-child").appendTo(".slider-2 ul");
+            $(".slider-2 ul").css('left', '0');
+        });
+    }
+
+    $("a.control-next-2").click(function (e) {
+        moveRight2();
+        return false;
+    });
+
+    $("a.control-pre-2").click(function (e) {
+        moveLeft2();
+        return false;
+    });
+// -------- /6 - slider 2 --------------
 
 
 // ------- 10 - form section - submit botton -------
 
-var name = document.getElementById("name");
-var email = document.getElementById("e-mail");
-var subject = document.getElementById("subject");
-var comment = document.getElementById("comment");
+    var name = document.getElementById("name");
+    var email = document.getElementById("e-mail");
+    var subject = document.getElementById("subject");
+    var comment = document.getElementById("comment");
 
-document.querySelector('form.user-info').addEventListener('submit', function (e) {
+    document.querySelector('form.user-info').addEventListener('submit', function (e) {
 
-    //prevent the normal submission of the form
-    e.preventDefault();
+        //prevent the normal submission of the form
+        e.preventDefault();
 
 
-    console.log(name.value);
-    console.log(email.value);
-    console.log(subject.value);
-    console.log(comment.value);
+        console.log(name.value);
+        console.log(email.value);
+        console.log(subject.value);
+        console.log(comment.value);
 
-});
+    });
 
 // ------- /10 - form section - submit botton -------
 
